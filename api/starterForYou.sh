14ac10cb859b0f3b4829a872318e60696d5befd5
Julien Guay <julien.sanghoan@gmail.com>
2020-10-09 22:12:48 -0400

    rencontreMathVendredi

--- api/starterForYou.sh
+++ api/starterForYou.sh
@@ -1,18 +1,2 @@
-sudo apt install npm
-npm init
-echo '{
-  "name": "farm-api",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index",
-    "dev": "nodemon index"
-},
-  "author": "",
-  "license": "ISC"
-}
-' > package.json
-
 npm install --save-dev nodemon
 npm install mysql
